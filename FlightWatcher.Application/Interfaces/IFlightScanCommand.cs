d6ee899566b0ce8e93e848809814828c12548185
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-09-19 15:02:58 +0300

    Initialize Playwright commands with provider search parameters

--- FlightWatcher.Application/Interfaces/IFlightScanCommand.cs
+++ FlightWatcher.Application/Interfaces/IFlightScanCommand.cs
@@ -1,7 +1,14 @@
+using FlightWatcher.Core.Models;
+
 namespace FlightWatcher.Application.Interfaces
 {
     public interface IFlightScanCommand
     {
-        Task Execute();
+        Task Execute(
+            string from,
+            string to,
+            DateOnly departureDay,
+            bool isOneWay,
+            IList<Passenger> passengers);
     }
 }
