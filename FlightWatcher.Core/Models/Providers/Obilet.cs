11bc4c1eff3d3c64eb4b707c81bf79c12b97f177
Emre Üçbudak <emreucbudak1905@gmail.com>
2026-09-19 16:54:49 +0300

    Build Obilet search URL with dates and passenger parameters

--- FlightWatcher.Core/Models/Providers/Obilet.cs
+++ FlightWatcher.Core/Models/Providers/Obilet.cs
@@ -1,3 +1,5 @@
+using FlightWatcher.Core.Enums;
+
 namespace FlightWatcher.Core.Models.Providers
 {
     public class Obilet
@@ -10,8 +12,28 @@ namespace FlightWatcher.Core.Models.Providers
 
         public DateOnly DepartureDay { get; set; }
 
+        public DateOnly? ReturnDay { get; set; }
+
         public bool IsOneWay { get; set; }
 
         public IList<Passenger> Passengers { get; set; } = new List<Passenger>();
+
+        public string[] GetPassengerParameters()
+        {
+            var parameters = new string[Passengers.Count];
+
+            for (var i = 0; i < Passengers.Count; i++)
+            {
+                var passenger = Passengers[i];
+                if (passenger.PersonType == PersonType.Adult)
+                    parameters[i] = passenger.PersonCount + "a";
+                else if (passenger.PersonType == PersonType.Student)
+                    parameters[i] = passenger.PersonCount + "s";
+                else
+                    parameters[i] = passenger.PersonCount + "c";
+            }
+
+            return parameters;
+        }
     }
 }
